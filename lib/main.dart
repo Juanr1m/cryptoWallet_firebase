69e2bf8908585e96917369522491090124ed053a
Rim Karimov <rimkarimov20@gmail.com>
2020-10-26 16:13:24 +0500

    authentication done

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:crypto_wallet/ui/authenication.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Crypto Wallet',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Authentication(),
+    );
+  }
+}
